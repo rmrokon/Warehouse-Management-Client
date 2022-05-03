ba79c1b220346bb2c5ba8aeb3f4e46428eac3bae
rmrokon <mdrokon7773@gmail.com>
2022-05-04 00:57:05 +0600

    Update quantity form and functionality done

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -3,9 +3,12 @@ import { Link, useParams } from 'react-router-dom';
 import useProductDetail from '../../hooks/useProductDetail';
 
 const ProductDetail = () => {
+    // const [displayQuantity, setDisplayQuantity] = useState(0);
     const { productId } = useParams();
     const [product, setProduct] = useProductDetail(productId);
     const { _id, name, img, description, supplier, price, quantity } = product;
+    // setDisplayQuantity(parseInt(quantity));
+
 
     const handleDecreaseQuantity = (id) => {
         const { quantity, ...rest } = product;
@@ -28,19 +31,48 @@ const ProductDetail = () => {
 
     }
 
+    const handleUpdateQuantity = (e) => {
+        e.preventDefault();
+        const inputQuantity = e.target.quantity.value;
+        const { quantity, ...rest } = product;
+        const previousQuantity = parseInt(quantity);
+        const currentQuantity = previousQuantity + parseInt(inputQuantity);
+        const updatedProduct = { currentQuantity, ...rest }
+
+        fetch(`http://localhost:5000/updateProduct/${_id}`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedProduct)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
 
     return (
-        <div className='bg-red-400'>
-            <div className='border-2 rounded-md bg-blue-300 w-6/12 mx-auto'>
-                <img className='border-2 w-1/4 rounded-md mx-auto my-3' src={img} alt="" />
-                <div className='p-3 text-center'>
-                    <h3 className='font-bold mb-3'>{name}</h3>
-                    <h4 className='mb-2'>Product Id: {_id}</h4>
-                    <h4 className='font-bold mb-2'>Price: ${price}</h4>
-                    <h4 className='mb-2 text-xl'>Stock Available: {quantity}</h4>
-                    <button onClick={() => handleDecreaseQuantity(_id)} className='bg-green-400 p-1 rounded text-white'>Delivered</button>
-                    <h4 className='mb-2'>Supplier: {supplier}</h4>
-                    <p>{description}</p>
+        <div className=''>
+            <div className='grid grid-cols-1 lg:grid-cols-2'>
+                <div className='border-2 rounded-md w-2/3 mx-auto'>
+                    <img className='border-2 w-1/4 rounded-md mx-auto my-3' src={img} alt="" />
+                    <div className='p-3 text-center'>
+                        <h3 className='font-bold mb-3'>{name}</h3>
+                        <h4 className='mb-2'>Product Id: {_id}</h4>
+                        <h4 className='font-bold mb-2'>Price: ${price}</h4>
+                        <h4 className='mb-2 text-xl'>Stock Available: {quantity}</h4>
+                        <button onClick={() => handleDecreaseQuantity(_id)} className='bg-green-400 p-1 rounded text-white'>Delivered</button>
+                        <h4 className='mb-2'>Supplier: {supplier}</h4>
+                        <p>{description}</p>
+                    </div>
+                </div>
+                <div className='flex justify-center place-items-center my-5'>
+                    <form className='w-full text-center' onSubmit={handleUpdateQuantity}>
+                        <input className='border w-2/3 rounded-md text-center h-16' type="number" name="quantity" placeholder='Enter Quantity' /> <br />
+                        <input className='bg-pink-600 p-3 rounded-lg text-white mt-2' type="submit" value="Update Quantity" />
+                    </form>
                 </div>
             </div>
             <div className='flex justify-center my-10'>
